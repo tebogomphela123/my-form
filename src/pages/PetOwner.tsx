10e90c8ad9815b90c5320056ddaef1fc35f5ddcd
tebmphel021 <tebmphel021@student.wethinkcode.co.za>
2023-03-25 12:34:17 +0200

    updated code

--- src/pages/PetOwner.tsx
+++ src/pages/PetOwner.tsx
@@ -1,49 +1,44 @@
 
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { HTMLAttributes } from 'react'
-import { useState } from "react";
-
 const inter = Inter({ subsets: ['latin'] })
-
 
 
 const PetOwnwer = () => {
 
     return(
+      <form action="/veterinary/contact" method="post">
 
-    <div className=''>
-      <div className="text-2xl text-black font-medium">Pet Owner Details</div>
-      <label htmlFor="firstName" className='comfortaa'>First Name <span className="asteric text-red-600">&#42;</span></label>
-      <input type="text" id="firstName" name="firstName" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' minLength={2} placeholder="First Name" required aria-required="true"/>
-  
-      <label htmlFor="lastName" className='comfortaa text-l'>Last Name <span className="asteric text-red-600">&#42;</span></label>
-      <input type="text" id="lastName" name="lastName" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder="Last Name"  required  minLength={2}/>
-  
-      <label htmlFor="email" className='comfortaa'>Email <span className="asteric text-red-600">&#42;</span></label>
-      <input type="email" id="email" name="email" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='esa@electricsheepagency.com' required/>
-  
-      <label htmlFor="phone" className='comfortaa'>Phone Number <span className="asteric text-red-600">&#42;</span></label>
-      <input type="tel" id="phone" name="phone" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' pattern="[0]{1}[0-9]{2}[0-9]{3}[0-9]{4}" placeholder='0820001111' minLength={10} maxLength={10} required/>
-  
-      <label htmlFor="address" className='comfortaa'>Address <span className="asteric text-red-600">&#42;</span></label>
-      <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='Street Address' required/>
-      <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='Street Address Line 2' required/>
+        <div className=''>
+          <div className="text-2xl text-black font-medium">Pet Owner Details</div>
+          <label htmlFor="firstName" className='comfortaa'>First Name <span className="asteric text-red-600">&#42;</span></label>
+          <input type="text" id="firstName" name="firstName" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' minLength={2} placeholder="First Name" required aria-required="true"/>
       
-      <div className='flex flex-wrap gap-2  '>
-          <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='City' required/>
-  
-          <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='State / Province' required/>
-      </div>
-      <div className='flex flex-wrap gap-2 '>
-        
-          <input type="tel" id='address' name='streetAddress' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' pattern="[0-9]{4}" minLength={4} maxLength={4}  placeholder='Postal / Zip Code' required/>
-  
-          <input type="text" id='address' name='streetAddress' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='Country' required/>
-      </div>
-    </div>
+          <label htmlFor="lastName" className='comfortaa text-l'>Last Name <span className="asteric text-red-600">&#42;</span></label>
+          <input type="text" id="lastName" name="lastName" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder="Last Name"  required  minLength={2}/>
+      
+          <label htmlFor="email" className='comfortaa'>Email <span className="asteric text-red-600">&#42;</span></label>
+          <input type="email" id="email" name="email" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='esa@electricsheepagency.com' required/>
+      
+          <label htmlFor="phone" className='comfortaa'>Phone Number <span className="asteric text-red-600">&#42;</span></label>
+          <input type="tel" id="phone" name="phone" className='border border-gray-300 blocl my-2  py-2 px-4 w-full rounded focus:online-none focus:border-teal' pattern="[0]{1}[0-9]{2}[0-9]{3}[0-9]{4}" placeholder='0820001111' minLength={10} maxLength={10} required/>
+      
+          <label htmlFor="address" className='comfortaa'>Address <span className="asteric text-red-600">&#42;</span></label>
+          <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='Street Address' required/>
+          <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 py-2 px-4 w-full rounded focus:online-none focus:border-teal' placeholder='Street Address Line 2' required/>
+          
+          <div className='flex flex-wrap gap-2  '>
+              <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='City' required/>
+      
+              <input type="text" id='address' name='address' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='State / Province' required/>
+          </div>
+          <div className='flex flex-wrap gap-2 '>
+            
+              <input type="tel" id='address' name='streetAddress' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' pattern="[0-9]{4}" minLength={4} maxLength={4}  placeholder='Postal / Zip Code' required/>
+      
+              <input type="text" id='address' name='streetAddress' className='border border-gray-300 blocl my-2 flex-grow py-2 px-4 min-w-[40%] rounded focus:online-none focus:border-teal' placeholder='Country' required/>
+          </div>
+        </div>
+      </form>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import PetOwnwer from './PetOwner'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
 export default function ContacForm() {
   const [step, setStep] = useState(1);
 
@@ -26,28 +25,36 @@ export default function ContacForm() {
           Thank you for considering Electric Sheep Veterinary Clinic for your pet's needs. Please fill out our new client/patient contact form in entirety to ensure we can provide you and your pet with the best possible care.
       </div>
       <hr />
-        <div className="flex mb-4">
-          <div className={`w-1/2 border-r border-gray-400 ${step === 1 ? "bg-blue-500 text-white" : "bg-gray-200" } p-2 text-center cursor-pointer`} onClick={() => setStep(1)}>
-            Step 1
+
+        <form method="POST" action="https://www.formbackend.com/f/664decaabbf1c319">
+          <div className="flex mb-4">
+            <div className={`w-1/2 border-r border-gray-400 ${step === 1 ? "bg-blue-500 text-white" : "bg-gray-200" } p-2 text-center cursor-pointer`} onClick={() => setStep(1)}>
+              Step 1
+            </div>
+            <div
+              className={`w-1/2 ${step === 2 ? "bg-blue-500 text-white" : "bg-gray-200" } p-2 text-center cursor-pointer`} onClick={() => setStep(2)}>
+              Step 2
+            </div>
           </div>
-          <div
-            className={`w-1/2 ${step === 2 ? "bg-blue-500 text-white" : "bg-gray-200" } p-2 text-center cursor-pointer`} onClick={() => setStep(2)}>
-            Step 2
+
+          {step === 1 ? <PetOwnwer /> : <Pet />}
+          <div className="flex justify-between mt-6">
+            {step > 1 && (
+              <button className="bg-gray-300 px-6 py-1.5 rounded-lg text-gray-700 hover:bg-gray-400" onClick={handleBack}>
+                Back
+              </button>
+              
+            )}
+            {step < 2 && (
+              <button className="bg-blue-500 px-6 py-1.5 rounded-lg text-white hover:bg-blue-600" onClick={handleNext}>
+                Next
+              </button>
+            
+            )}
           </div>
-        </div>
-        {step === 1 ? <PetOwnwer /> : <Pet />}
-        <div className="flex justify-between mt-6">
-          {step > 1 && (
-            <button className="bg-gray-300 px-6 py-1.5 rounded-lg text-gray-700 hover:bg-gray-400" onClick={handleBack}>
-              Back
-            </button>
-          )}
-          {step < 2 && (
-            <button className="bg-blue-500 px-6 py-1.5 rounded-lg text-white hover:bg-blue-600" onClick={handleNext}>
-              Next
-            </button>
-          )}
-        </div>
+          
+          
+        </form>
       </div>
     </div>
   );
